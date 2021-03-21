479141791c69b032759844f270166da9d380b479
Nagy Zoltán <zoli1984@gmail.hu>
2021-03-21 17:09:14 +0100

    ..

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DomesticCatComponent} from '..//app/domestic-cat/domestic-cat.component';
+import {WildcatComponent} from '..//app/wildcat/wildcat.component';
+import {BobCatComponent} from '..//app/bob-cat/bob-cat.component';
+import {LionComponent} from '..//app/lion/lion.component';
+import {LeopardComponent} from '../app/leopard/leopard.component';
+import {PantherComponent} from '..//app/panther/panther.component';
+import {GepardComponent} from '..//app/gepard/gepard.component';
+import {PumaComponent} from '..//app/puma/puma.component';
 
 
-const routes: Routes = [];
+
+
+
+
+
+
+const routes: Routes = [
+  
+ 
+{path:'domestic-cat',  component:DomesticCatComponent},
+{path:'wildcat', component:WildcatComponent},
+{path:'bob-cat', component:BobCatComponent},
+{path:'lion', component:LionComponent},
+{path:'leopard', component:LeopardComponent},
+{path:'panther', component:PantherComponent},
+{path:'gepard', component:GepardComponent},
+{path:'puma', component:PumaComponent},
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
